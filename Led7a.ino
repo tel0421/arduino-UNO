9659a52d102d62ee1f99c964f4c8d44be8e25853
tel0421 <52900421+tel0421@users.noreply.github.com>
2019-08-28 13:41:52 +0800

    Add files via upload

---
+++ Led7a.ino
@@ -0,0 +1,53 @@
+#include "SwitchButton.h"
+SwitchButton Btn(12);
+//***********************************************************
+//參考的網址
+//http://programmermagazine.github.io/201307/htm/article1.html
+//***********************************************************
+
+
+byte digits[10][7] = {
+  { 1, 1, 1, 1, 1, 1, 0 }, // = 0
+  { 0, 1, 1, 0, 0, 0, 0 }, // = 1
+  { 1, 1, 0, 1, 1, 0, 1 }, // = 2
+  { 1, 1, 1, 1, 0, 0, 1 }, // = 3
+  { 0, 1, 1, 0, 0, 1, 1 }, // = 4
+  { 1, 0, 1, 1, 0, 1, 1 }, // = 5
+  { 1, 0, 1, 1, 1, 1, 1 }, // = 6
+  { 1, 1, 1, 0, 0, 0, 0 }, // = 7
+  { 1, 1, 1, 1, 1, 1, 1 }, // = 8
+  { 1, 1, 1, 0, 0, 1, 1 } // = 9
+};
+
+byte mapArdiuinoPin[] = {2, 3, 4, 5, 6, 7, 8};
+
+void setup() {
+  Serial.begin(115200);
+  for (byte i = 0; i < 7; i++) {
+    pinMode(mapArdiuinoPin[i], OUTPUT);
+  }
+}
+
+void loop() {
+  unsigned int stateChangeCount = btn.getStateChangeCount();
+  if (Serial.available() > 0) {
+    char getDigit = Serial.read();
+    if (getDigit >= 48 && getDigit <= 57) {
+      Serial.println(getDigit);
+      displayDigit(getDigit);
+    } else {
+      Serial.println("error");
+    }
+
+  }
+}
+
+
+void displayDigit(char displayDigit){
+  int displayIndex = (int)displayDigit - 48;
+  for(int i=0; i<7 ; i++){
+    bool onePinState = digits[displayIndex][i];
+    byte pinNum = mapArdiuinoPin[i];
+    digitalWrite(pinNum, onePinState);
+  }
+}
